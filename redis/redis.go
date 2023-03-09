704c02132249fe0dc9d04cc7d6b78ddc0937b597
Igor Mykhailov <igor.mykhailov@sprious.com>
2023-03-09 17:56:11 +0200

    Add Go module redis

---
+++ redis/redis.go
@@ -0,0 +1,103 @@
+package redis
+
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	redisConnType = "unix"
+	redisAddr = "/var/run/redis/redis.sock"
+	redisDB = 1
+)
+
+var (
+	Pool *redis.Pool
+)
+
+func init() {
+	Pool = newPool()
+	cleanupHook()
+}
+
+func newPool() *redis.Pool {
+
+	return &redis.Pool{
+
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial(redisConnType, redisAddr)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func Ping() error {
+
+	conn := Pool.Get()
+	defer conn.Close()
+
+	_, err := redis.String(conn.Do("PING"))
+	if err != nil {
+		return fmt.Errorf("cannot 'PING' db: %v", err)
+	} else {
+		fmt.Printf("Redis PING OK")
+	}
+	return nil
+}
+
+func Get(key string) ([]byte, error) {
+
+	conn := Pool.Get()
+	defer conn.Close()
+
+	var data []byte
+	data, err := redis.Bytes(conn.Do("GET", key))
+	if err != nil {
+		return data, fmt.Errorf("error getting key %s: %v", key, err)
+	}
+	return data, err
+}
+
+func Set(key string, value []byte) error {
+
+	conn := Pool.Get()
+	defer conn.Close()
+
+	_, err := conn.Do("SET", key, value)
+	if err != nil {
+		v := string(value)
+		if len(v) > 15 {
+			v = v[0:12] + "..."
+		}
+		return fmt.Errorf("error setting key %s to %s: %v", key, v, err)
+	}
+	return err
+}
+
+func cleanupHook() {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	signal.Notify(c, syscall.SIGKILL)
+	go func() {
+		<-c
+		Pool.Close()
+		os.Exit(0)
+	}()
+}
